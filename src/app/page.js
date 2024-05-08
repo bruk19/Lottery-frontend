870e61aa3044f69cf4182e633e3371c184ae90fb
Bruk Teshome <brukteshome21@gmail.com>
2024-05-08 17:25:39 +0300

    Create header component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
+import Header from "../../components/Header";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+          <Header />
         <h1>Hello world</h1>
       </div>
     </main>
