ffeedf5e4ed64ec6a8016d46f178c441211297b8
Bruk Teshome <brukteshome21@gmail.com>
2024-05-09 15:39:28 +0300

    set a local storage for a connected account

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,29 @@
-import React from 'react'
+import { React, useEffect } from 'react'
 import { useMoralis } from "react-moralis"
 
 function Header() {
-  const { enableWeb3, account } = useMoralis()
+  const { enableWeb3, account, isWeb3Enabled } = useMoralis()
+
+  useEffect(() => {
+    if (isWeb3Enabled) return 
+    if (typeof window !== "undefined") {
+      if (window.localStorage.getItem("connected")) {
+          enableWeb3()
+      }
+    }
+  
+  }, [isWeb3Enabled])
+
   return (
     <div>
-      {account ? (<div>Connected</div>) : (<button onClick={async () => { await enableWeb3() }}>Connect</button>)}
-
+      {account ? (<div>Connected to {account.slice(0, 4)}...{account.length - 4}</div>) : (
+        <button onClick={
+          async () => {
+            await enableWeb3()
+            if (typeof window !== "undefined") {
+              window.localStorage.setItem("connected", "inject")
+            }
+          }}>Connect</button>)}
     </div>
   )
 }
