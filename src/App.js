9c830a399cdeea11e2874de6ee15cd217aa5ccfb
Bruk Teshome <brukteshome21@gmail.com>
2024-05-09 21:25:50 +0300

    Create lottery intrance function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { MoralisProvider } from "react-moralis";
 import HeaderKit from "./components/HeaderKit";
+import LotteryIntrance from "./components/LotteryIntrance";
 // import Header from "./Header";
 function App() {
   return (
     <MoralisProvider initializeOnMount={false} >
       <HeaderKit />
+      <LotteryIntrance />
     </MoralisProvider>
   );
 }
