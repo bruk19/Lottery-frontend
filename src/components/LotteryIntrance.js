4ead35ad55d93443ab17eadd94ebbefd54f86052
Bruk Teshome <brukteshome21@gmail.com>
2024-05-13 22:35:43 +0300

    Add useWeb3Contract functionality from react moralis

--- src/components/LotteryIntrance.js
+++ src/components/LotteryIntrance.js
@@ -1,6 +1,16 @@
-import React from 'react'
+import React from 'react';
+import { useWeb3Contract } from "react-moralis";
 
 export default function LotteryIntrance() {
+  const {
+    runContractFunction: enterRaffle
+  } = useWeb3Contract({
+    abi: //,
+    contractAddres: //,
+    functionName: //,
+    params: {},
+    msgValue: //
+  })
   return (
     <div>LotteryIntrance</div>
   )
